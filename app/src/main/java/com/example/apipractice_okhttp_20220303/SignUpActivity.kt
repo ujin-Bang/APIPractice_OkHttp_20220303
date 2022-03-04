276f53676ffc8d150b44025201ba19ae49198b0f
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-04 16:51:07 +0900

    회원가입 API 실제 사용 - 응답 처리는 하지 않은 상태

--- app/src/main/java/com/example/apipractice_okhttp_20220303/SignUpActivity.kt
+++ app/src/main/java/com/example/apipractice_okhttp_20220303/SignUpActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.apipractice_okhttp_20220303.databinding.ActivitySignUpBinding
+import com.example.apipractice_okhttp_20220303.utils.ServerUtil
+import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
 
@@ -24,6 +26,17 @@ class SignUpActivity : BaseActivity() {
             val inputPw = binding.edtPassword.text.toString()
             val inputNickname = binding.edtNickname.text.toString()
 
+            ServerUtil.putRequestSignUp(
+                inputEmail,
+                inputPw,
+                inputNickname,
+                object :ServerUtil.JsonResponseHandler{
+                    override fun onResponse(jsonObj: JSONObject) {
+
+                    }
+
+                }
+            )
 
         }
 
