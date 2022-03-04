fd1f79827a97a13335381d0fe666ad1465391c51
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-04 15:12:59 +0900

    추상메쏘드 2개를 물려주는 클래스 생성. BaseActivity를 물려받아 의무구현 함수 2개  - 추상 클래스로 생성(xml은 만들필요 없다)/상속만을 위해서 존재함  -onCreate에서 실제 실행

--- app/src/main/java/com/example/apipractice_okhttp_20220303/SignUpActivity.kt
+++ app/src/main/java/com/example/apipractice_okhttp_20220303/SignUpActivity.kt
@@ -5,14 +5,24 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.apipractice_okhttp_20220303.databinding.ActivitySignUpBinding
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : BaseActivity() {
 
     lateinit var binding: ActivitySignUpBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       binding = DataBindingUtil.setContentView(this,R.layout.activity_sign_up)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_sign_up)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
 
     }
+
+    override fun setValues() {
+
+    }
+
 
 }
