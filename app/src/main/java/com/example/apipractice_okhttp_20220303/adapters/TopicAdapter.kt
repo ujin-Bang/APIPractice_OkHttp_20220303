dd59956289dea7e06e114d930f49ad588c0cfe9b
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-07 17:20:21 +0900

    리스트뷰에 실제 데이터 넣기

--- app/src/main/java/com/example/apipractice_okhttp_20220303/adapters/TopicAdapter.kt
+++ app/src/main/java/com/example/apipractice_okhttp_20220303/adapters/TopicAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.example.apipractice_okhttp_20220303.R
 import com.example.apipractice_okhttp_20220303.datas.TopicData
 
@@ -22,6 +23,12 @@ class TopicAdapter(
         }
 
         val row = tempRow!!
+
+        val data = mList[position]
+
+        val txtTitle = row.findViewById<TextView>(R.id.txtTitle)
+
+        txtTitle.text = data.title
 
         return row
 
