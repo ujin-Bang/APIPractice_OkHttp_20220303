d783030784a04ff614d383fefc16d338df2886d6
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-08 09:53:12 +0900

    2.5초 뒤에 진행할 검사 주석으로 남김

--- app/src/main/java/com/example/apipractice_okhttp_20220303/SplashActivity.kt
+++ app/src/main/java/com/example/apipractice_okhttp_20220303/SplashActivity.kt
@@ -2,6 +2,8 @@ package com.example.apipractice_okhttp_20220303
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : BaseActivity() {
 
@@ -17,6 +19,25 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        2.5초가 지나면 -> 자동로그인을 해도 되는지? -> 상황에 맞는 화면으로 이동
+
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed({
+
+//            자동로그인을 해도 되는가?
+//            1)사용자가 자동로그인 의사를 OK 했는지?
+//            2)로그인 시에 받아낸 토큰값이 지금도 유효한지?
+          if(){
+
+
+//              둘다 OK라면 바로 메인화면으로
+        }
+            else{
+
+        }
+    }  ,2500)
 
     }
 
