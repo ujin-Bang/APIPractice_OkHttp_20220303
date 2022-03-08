d2d84ba8829dd85f97fe7de37c415d2108f82fae
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-08 16:52:07 +0900

    UserData 클래스 추가 + ReplyData 파싱에 활용  - 리스트뷰에 사용할 데이터 클래스 준비

--- app/src/main/java/com/example/apipractice_okhttp_20220303/datas/ReplyData.kt
+++ app/src/main/java/com/example/apipractice_okhttp_20220303/datas/ReplyData.kt
@@ -7,6 +7,8 @@ class ReplyData(
     var content: String,
 ) {
 
+    var writer = UserData() //모든 댓글에는 작성자가 있다. null가능성이 없다.
+
 //    보조생성자 추가 연습 : 파라미터 없는 형태
     constructor() : this( 0, "내용없음")
 
@@ -18,6 +20,9 @@ class ReplyData(
             val replyData = ReplyData()
 
 //                JSON 정보 > 멤버변수 채우기
+            replyData.id = jsonObj.getInt("id")
+            replyData.content = jsonObj.getString("content")
+            replyData.writer = UserData.getUserDataFromJon(jsonObj.getJSONObject("user"))
 
             return replyData
 
---
+++ app/src/main/java/com/example/apipractice_okhttp_20220303/datas/UserData.kt
@@ -0,0 +1,24 @@
+package com.example.apipractice_okhttp_20220303.datas
+
+import org.json.JSONObject
+
+class UserData {
+
+    var id = 0
+    var email =""
+    var nickname = ""
+
+    companion object {
+        fun getUserDataFromJon( jsonObj: JSONObject ) : UserData {
+
+            val userData = UserData()
+
+            userData.id = jsonObj.getInt("id")
+            userData.email = jsonObj.getString("email")
+            userData.nickname = jsonObj.getString("nick_name")
+
+            return userData
+
+        }
+    }
+}
