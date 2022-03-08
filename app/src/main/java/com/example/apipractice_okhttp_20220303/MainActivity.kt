c6260d89391448d50a60b96fbe8c5bf50dae0ac6
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-08 10:40:52 +0900

    토론 주제 데이터를 들고, 상세화면 진입

--- app/src/main/java/com/example/apipractice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/example/apipractice_okhttp_20220303/MainActivity.kt
@@ -35,6 +35,10 @@ class MainActivity : BaseActivity() {
 
             val clickedTopic = mTopicList[poistion]
 
+            val myIntent = Intent(mContext, ViewTopicDetailActivity::class.java)
+            myIntent.putExtra("topic", clickedTopic)
+            startActivity(myIntent)
+
         }
 
         binding.btnLogout.setOnClickListener {
--- app/src/main/java/com/example/apipractice_okhttp_20220303/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/example/apipractice_okhttp_20220303/ViewTopicDetailActivity.kt
@@ -4,13 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.apipractice_okhttp_20220303.databinding.ActivityViewTopicDetailBinding
+import com.example.apipractice_okhttp_20220303.datas.TopicData
 
-class ViewTopicDetailActivity : AppCompatActivity() {
+class ViewTopicDetailActivity : BaseActivity() {
 
     lateinit var binding : ActivityViewTopicDetailBinding
 
+//    보여주게 될 토론 주제 데이터 > 이벤트처리, 데이터 표현 등 여러 함수에서 사용
+    lateinit var mTopicData : TopicData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       binding = DataBindingUtil.setContentView(this, R.layout.activity_view_topic_detail)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_view_topic_detail)
+        mTopicData = intent.getSerializableExtra("topic") as TopicData
+        setupEvents()
+        setValues()
     }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+
+
 }
