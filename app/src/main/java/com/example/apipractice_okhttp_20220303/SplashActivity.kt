bc064bff36a4b1bf8a2bdfd4239863294a975332
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-08 10:09:52 +0900

    자동로그인 여부 검사에 따른 Intent대입만 분기 처리

--- app/src/main/java/com/example/apipractice_okhttp_20220303/SplashActivity.kt
+++ app/src/main/java/com/example/apipractice_okhttp_20220303/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.example.apipractice_okhttp_20220303
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -23,6 +24,23 @@ class SplashActivity : BaseActivity() {
 
     override fun setValues() {
 
+//        2.5초가 지나기 전에 미리 사용자 정보 조회 시도(토큰 유효성 검증)
+        var isTokenOk = false // 검사를 통과하면 true로 변경 예정.
+        ServerUtil.getRequestMainInfo(mContext, object : ServerUtil.JsonResponseHandler{
+            override fun onResponse(jsonObj: JSONObject) {
+
+//                    내 정보를 잘 가져왔다면? => code값을 200dmfh sofuwnsek.
+
+//                    200이 아니라면? 무슨 이유던 사용자 정보 로드 실패. (토큰 유효X)
+
+                val code = jsonObj.getInt("code")
+
+                isTokenOk = (code == 200)
+            }
+
+        })
+
+
 //        2.5초가 지나면 -> 자동로그인을 해도 되는지? -> 상황에 맞는 화면으로 이동
 
         val myHandler = Handler(Looper.getMainLooper())
@@ -36,33 +54,25 @@ class SplashActivity : BaseActivity() {
 
 //            2)로그인 시에 받아낸 토큰값이 지금도 유효한지?
 
-            var isTokenOk = false // 검사를 통과하면 true로 변경 예정.
-
 //            2-1) 저장된 토큰이 있는지부터 확인(임시 방편)
 //            2-2) 그 토큰이 사용자 정보를 실제로 불러내는지 확인(실제 방편)
+//              =>2.5초 전에 미리 물어본 상태. isTokenOk에 결과가 들어있다.
 
-            ServerUtil.getRequestMainInfo(mContext, object : ServerUtil.JsonResponseHandler{
-                override fun onResponse(jsonObj: JSONObject) {
+//          Intent로 화면이동 => 상황에 따라 다른 Intent를 만든다.
+            val myIntent : Intent
 
-//                    내 정보를 잘 가져왔다면? => code값을 200dmfh sofuwnsek.
+          if(userAutoLoin && isTokenOk){
 
-//                    200이 아니라면? 무슨 이유던 사용자 정보 로드 실패. (토큰 유효X)
+//              둘다 OK라면 바로 메인화면으로
+              myIntent = Intent(mContext,MainActivity::class.java)
+        }
+            else {
+//                아니라면, 로그인 화면으로
+                myIntent = Intent(mContext, LoginActivity::class.java)
+          }
 
-                    val code = jsonObj.getInt("code")
-
-                    isTokenOk = (code == 200)
-                }
-
-            })
-
-//          if(){
-//
-//
-////              둘다 OK라면 바로 메인화면으로
-//        }
-//            else{
-//
-//        }
+        startActivity(myIntent)
+            finish()
     }  ,2500)
 
     }
