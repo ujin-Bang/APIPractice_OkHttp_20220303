f116986c751046e4867b8ea09adefcf0f3391c1f
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-08 11:54:08 +0900

    JSON을 넣으면 > TopicData로 변환해주는 기능 추가 / 활용

--- app/src/main/java/com/example/apipractice_okhttp_20220303/datas/TopicData.kt
+++ app/src/main/java/com/example/apipractice_okhttp_20220303/datas/TopicData.kt
@@ -1,5 +1,6 @@
 package com.example.apipractice_okhttp_20220303.datas
 
+import org.json.JSONObject
 import java.io.Serializable
 
 class TopicData : Serializable {
@@ -8,4 +9,28 @@ class TopicData : Serializable {
     var title = "" //title은 String이라고 명시.
     var imageURL = "" //서버에서는 img_url로 주지만 앱에서는 imageURL로 받겠다. 변수명 다른 경우
     var replyCount = 0
+
+
+    companion object {
+
+        //    주제 정보를 담고 있는 JSONObject가 들어오면 > TopicData형태로 변환해주는 함수. => static메쏘드로 만들자.
+
+        fun getTopicDataFromJson( jsonObj: JSONObject) :TopicData {
+
+//            기본 내용의 TopicData 생성
+            val topicData = TopicData()
+
+//            jsonObj에서 데이터 추출 > 멤버변수 대입
+            topicData.id = jsonObj.getInt("id")
+            topicData.title = jsonObj.getString("title")
+            topicData.imageURL = jsonObj.getString("img_url")
+            topicData.replyCount = jsonObj.getInt("reply_count")
+
+//            완성된 TopicData 리턴
+
+            return topicData
+        }
+
+    }
+
 }
