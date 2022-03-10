122b936953cc4913f97a2480808711163122ba1f
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-10 12:21:18 +0900

    실제 토론 주제 데이터를 넘겨서 제목 / 진영의 제목 표시

--- app/src/main/java/com/example/apipractice_okhttp_20220303/EditReplyActivity.kt
+++ app/src/main/java/com/example/apipractice_okhttp_20220303/EditReplyActivity.kt
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.apipractice_okhttp_20220303.databinding.ActivityEditReplyBinding
+import com.example.apipractice_okhttp_20220303.datas.TopicData
 
 class EditReplyActivity : BaseActivity() {
 
     lateinit var binding : ActivityEditReplyBinding
 
+//    어느 토론 주제에서 온건지 + 무조건 선택한 진영도 있다.
+    lateinit var mTopicData : TopicData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
       binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_reply)
+        mTopicData = intent.getSerializableExtra("topic") as TopicData
         setupEvents()
         setValues()
     }
@@ -20,6 +25,9 @@ class EditReplyActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        binding.txtTopicTitle.text = mTopicData.title
+        binding.txtSideTitle.text = mTopicData.mySelectedSide!!.title
 
     }
 
