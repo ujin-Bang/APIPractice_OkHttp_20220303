107c5cfda61467c6082bea47e1cbeac265b79304
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-10 09:56:06 +0900

    Calendar의 내부 일시값 변경 연습

--- app/src/main/java/com/example/apipractice_okhttp_20220303/datas/ReplyData.kt
+++ app/src/main/java/com/example/apipractice_okhttp_20220303/datas/ReplyData.kt
@@ -1,6 +1,7 @@
 package com.example.apipractice_okhttp_20220303.datas
 
 import org.json.JSONObject
+import java.util.*
 
 class ReplyData(
     var id: Int,
@@ -10,6 +11,10 @@ class ReplyData(
     var writer = UserData() //모든 댓글에는 작성자가 있다. null 가능성이 없다.
 
     var selectedSide = SideData()//모든 댓글에는 선택한 진영이 있다. null 가능성이 없다.
+
+//    작성일시를 담아둘 변수
+//    일/시 데이터를 변경 => 내부의 숫자만 변경. 변수에 새 객체 대입X. =>val로 써도 됨.
+    val createdAt = Calendar.getInstance()
 
 //    보조생성자 추가 연습 : 파라미터 없는 형태
     constructor() : this( 0, "내용없음")
@@ -27,6 +32,14 @@ class ReplyData(
             replyData.writer = UserData.getUserDataFromJon(jsonObj.getJSONObject("user"))
 
             replyData.selectedSide = SideData.getSideDataFromJon( jsonObj.getJSONObject("selected_side") )
+
+//            Calendar로 되어있는 작성일시의 시간을 서버가 알려주는 댓글 작성 일시로 맞춰줘야 함.
+
+//            임시 2022-01-12 10:55:35로 변경 (한번에 모두 변경)
+        replyData.createdAt.set(2022,1,12,10,55,35)
+
+//            임시2) 연돋만 2021년으로 변경 (항목을 찍어서 변경)
+            replyData.createdAt.set( Calendar.YEAR, 2021)
 
             return replyData
 
