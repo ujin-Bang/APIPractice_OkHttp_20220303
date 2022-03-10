9edd053034857441944587eb014b0161f1f91122
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-10 10:46:07 +0900

    SimpleDateFormat양식 연습문제

--- app/src/main/java/com/example/apipractice_okhttp_20220303/adapters/ReplyAdapter.kt
+++ app/src/main/java/com/example/apipractice_okhttp_20220303/adapters/ReplyAdapter.kt
@@ -43,8 +43,18 @@ class ReplyAdapter(
 //        월은 1작게 나옴. +1로 보정해줘야 함
 //        txtCreateAt.text = "${data.createdAt.get(Calendar.YEAR)}-${data.createdAt.get(Calendar.MONTH) +1}-${data.createdAt.get(Calendar.DAY_OF_MONTH)}"
 
-//        임시 2 - "2022-03-10" 형태로 표현 => SimpleDateFormat 활용
-        val sdf = SimpleDateFormat("yyyy-MM-dd")
+//        임시 2 - "22.03.10" 형태로 표현 => SimpleDateFormat 활용
+
+//        연습
+//        양식1) 2022년 3월 5일
+//        양식2) 220305
+//        양식3) 3월 5일 오전 2시 5분
+//        양식4) 21년 3/5 (토) - 18:05
+
+//        val sdf = SimpleDateFormat("yyyy년 M월 d일")
+//        val sdf = SimpleDateFormat("yyMMdd")
+//        val sdf = SimpleDateFormat("M월 d일 a h시 m분")
+        val sdf = SimpleDateFormat("yy년 M/d (E) - HH:mm")
 
 //        sdf.format(Date객체) => 지정해둔 양식의 String으로 가공
 //          createdAt : Calendar / format의 파라미터 :Date => Calendaq의 내용물인 time 변수가 Date
