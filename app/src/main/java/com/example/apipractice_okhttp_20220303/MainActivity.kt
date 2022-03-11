ac95c6adeb1628f4e114c83aefcfbe0553dfd8cb
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-11 10:36:31 +0900

    임시 - 메인화면의 액션바를 변경

--- app/src/main/java/com/example/apipractice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/example/apipractice_okhttp_20220303/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import com.example.apipractice_okhttp_20220303.adapters.TopicAdapter
 import com.example.apipractice_okhttp_20220303.databinding.ActivityMainBinding
@@ -75,6 +76,9 @@ class MainActivity : BaseActivity() {
         defaultActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
 
         defaultActionBar.setCustomView(R.layout.my_custom_action_bar)
+
+        val toolbar = defaultActionBar.customView.parent as Toolbar
+        toolbar.setContentInsetsAbsolute(0,0)
 //        메인 화면 정보 가져오기 => API 호출 / 응답 처리.
 //        코드상으로는 먼저 실행시키지만 완료는 어댑터 연결보다 늦을 수도 있다.
 //         => 목록에 토론 주제 추가 : 어댑터 연결 이후 추가
