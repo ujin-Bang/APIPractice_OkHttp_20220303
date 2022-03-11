16a2fe4ad61d3d76853adfb947721182790fbe71
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-11 10:29:44 +0900

    커스텀 액션바 - UI xml 추가

--- app/src/main/java/com/example/apipractice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/example/apipractice_okhttp_20220303/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import com.example.apipractice_okhttp_20220303.adapters.TopicAdapter
@@ -68,6 +69,12 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
+//        임시테스트 > 액션바 커스텀 모드
+
+        val defaultActionBar = supportActionBar!!
+        defaultActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+
+        defaultActionBar.setCustomView(R.layout.my_custom_action_bar)
 //        메인 화면 정보 가져오기 => API 호출 / 응답 처리.
 //        코드상으로는 먼저 실행시키지만 완료는 어댑터 연결보다 늦을 수도 있다.
 //         => 목록에 토론 주제 추가 : 어댑터 연결 이후 추가
